db987ab1c54569c0780981b11ca3e3df3778604e
Karl Jensen <karljensen@gmail.com>
2021-11-18 09:15:49 -0800

    Make the header link a button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,10 @@ const Index = () => {
 
   return (
     <main className="h-full flex flex-col">
-      <header
-        className="px-4 py-2 text-2xl text-white font-black bg-red-400 border-b border-red-200"
-        onClick={() => navigate("/")}
-      >
-        pomodooroo.
+      <header className="px-4 py-2 text-2xl text-white bg-red-400 border-b border-red-200">
+        <button className="font-black" onClick={() => navigate("/")}>
+          pomodooroo.
+        </button>
       </header>
       <section className="px-4 py-4 flex-grow bg-gray-100 flex flex-col">
         <div className="mx-auto">
